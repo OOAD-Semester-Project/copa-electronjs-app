76e432737445ad0692b1710a24d9dd932f0099a0
Madhusudhan Aithal <madhuaithal@Madhusudhans-MacBook-Pro.local>
2020-04-28 02:02:19 -0600

    Added the keycloak config in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,15 @@
+
+const keycloakConfig = {
+  url: 'https://copa-keycloak.herokuapp.com/auth',
+  // url: 'http://localhost:8080/auth',
+  realm: 'copa',
+  clientId: 'angular-client'
+};
 export const environment = {
   production: true,
-  // keycloak: keycloakConfig,  
+  keycloak: keycloakConfig,  
   clipboardserver: {
-    // baseUrl: "http://localhost:3000"
+    // baseUrl: "http://localhost:3010"
     baseUrl: "https://clipboard-syncronization-app.appspot.com"
   }
 };
